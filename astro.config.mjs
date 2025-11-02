ec27add0bd57a557c0632654fea46242d63433fb
Leonardo Fochesato Michelon <fmleonardo@protonmail.com>
2025-11-02 16:59:49 -0300

    feature: implement homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: { locales: ["pt-br", "en"], defaultLocale: "pt-br" },
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
 
   integrations: [react()],
 });
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,55 @@
+import { Button } from "./ui/button";
+
+type NavbarProps = {
+  currentLanguage: "pt-br" | "en";
+};
+
+const dict = {
+  "pt-br": {
+    home: "Início",
+    blog: "Blog",
+    projects: "Projetos",
+    contact: "Contato",
+  },
+  en: {
+    home: "Home",
+    blog: "Blog",
+    projects: "Projects",
+    contact: "Contact",
+  },
+};
+
+export const Navbar: React.FC<NavbarProps> = ({ currentLanguage }) => {
+  return (
+    <nav>
+      <ul className="flex flex-row justify-between m-4 ">
+        {/*Left*/}
+        <div>
+          <li>
+            <Button asChild>
+              <a href="/">{dict[currentLanguage].home}</a>
+            </Button>
+          </li>
+        </div>
+        {/*Right*/}
+        <div className="flex flex-col md:flex-row gap-4">
+          <li>
+            <Button asChild>
+              <a href="/blog">{dict[currentLanguage].blog}</a>
+            </Button>
+          </li>
+          <li>
+            <Button asChild>
+              <a href="/projetos">{dict[currentLanguage].projects}</a>
+            </Button>
+          </li>
+          <li>
+            <Button asChild>
+              <a href="/contato">{dict[currentLanguage].contact}</a>
+            </Button>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
+};
