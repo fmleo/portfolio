c87a06115931c76a04da030e9c0ee299e1fc46e1
Leonardo Fochesato Michelon <fmleonardo@protonmail.com>
2025-11-05 20:02:03 -0300

    feature: adjust code to score better at google's lighthouse

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,43 +2,27 @@ import { Button } from "./ui/button";
 
 export const Navbar: React.FC = () => {
   return (
-    <nav>
-      <ul className="flex flex-row justify-between m-4 ">
-        {/*Left*/}
-        <div>
-          <li>
-            <Button asChild>
-              <a href="/">Início</a>
-            </Button>
-          </li>
-        </div>
-        {/*Right*/}
-        <div className="flex flex-col md:flex-row gap-4">
-          {/*<li>
-            <Button asChild className="">
-              {currentLanguage === "en" ? (
-                <a href="/">Ver em português</a>
-              ) : (
-                <a href="/en">See in english</a>
-              )}
-            </Button>
-          </li>
-          <li>
-            <Button asChild variant="noShadow">
-              <span className="select-none">{dict[currentLanguage].blog}</span>
-            </Button>
-          </li>*/}
-          <li>
-            <Button asChild>
-              <a href="/projetos">Projetos</a>
-            </Button>
-          </li>
-          <li>
-            <Button asChild>
-              <a href="/contato">Contato</a>
-            </Button>
-          </li>
-        </div>
+    <nav className="flex flex-row justify-between m-4">
+      {/*Left*/}
+      <ul>
+        <li>
+          <Button asChild>
+            <a href="/">Início</a>
+          </Button>
+        </li>
+      </ul>
+      {/*Right*/}
+      <ul className="flex flex-col md:flex-row gap-4">
+        <li>
+          <Button asChild>
+            <a href="/projetos">Projetos</a>
+          </Button>
+        </li>
+        <li>
+          <Button asChild>
+            <a href="/contato">Contato</a>
+          </Button>
+        </li>
       </ul>
     </nav>
   );
