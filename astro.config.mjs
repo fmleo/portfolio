5d31c7a18e46fed97a143dec9a91fdb5a85e8212
fmleo <fmleonardo@protonmail.com>
2025-11-07 22:48:03 -0300

    feature: use astro-icon with svg instead of devicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,5 +18,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [react(), sitemap()],
+  integrations: [react(), sitemap(), icon()],
 });
