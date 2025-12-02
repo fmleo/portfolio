9b81c11ba0f41a37a9eec4b809fbed0bde21de3e
fmleo <fmleonardo@protonmail.com>
2025-12-02 10:43:18 -0300

    feature: add new logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,30 @@
 import { Button } from "./ui/button";
 
+import logoDark from "../assets/lfm-logo-dark.svg";
+import logoLight from "../assets/lfm-logo-light.svg";
+
 export const Navbar: React.FC = () => {
   return (
-    <nav className="flex flex-row justify-between m-4">
+    <nav className="flex flex-row justify-between m-4 items-center">
       {/*Left*/}
       <ul>
         <li>
-          <Button asChild>
-            <a href="/">Início</a>
-          </Button>
+          <a href="/" className="mx-4">
+            <img
+              src={logoDark.src}
+              alt="LFM Logo"
+              className="h-8 dark:hidden"
+            />
+            <img
+              src={logoLight.src}
+              alt="LFM Logo"
+              className="h-8 hidden dark:block"
+            />
+          </a>
         </li>
       </ul>
       {/*Right*/}
-      <ul className="flex flex-col md:flex-row gap-4">
+      <ul className="flex flex-col md:flex-row gap-4 items-end md:items-center">
         <li>
           <Button asChild>
             <a href="/projetos">Projetos</a>
